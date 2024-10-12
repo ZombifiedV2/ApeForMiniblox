9a676263982d28e3aa61620b1a71703feb420e8e
ZombifiedV2 <138331645+ZombifiedV2@users.noreply.github.com>
2024-10-12 16:59:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // @namespace    http://7granddadpgn.github.io
 // @version      2024-07-29
 // @description  A browser script made to give enhancements on miniblox.io
-// @author       7GrandDad - ManFacify
+// @author       7GrandDad - ManFaceExploits
 // @match        https://miniblox.io/*
 // @icon         https://miniblox.io/favicon.png
 // @grant        GM_getValue
